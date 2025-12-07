39f8536cb34ceff3de777af9c8f99f720f8ccabf
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-12-07 18:12:16 -0300

    Wip: Profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { SessionProviderWrapper } from "@/config/SessionProvider";
 import "./globals.css"; // Make sure you have this file
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { de } from "zod/locales";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+        <SessionProviderWrapper>
+          <div> 
+            <Header />
+            {children}
+          </div>
+          </SessionProviderWrapper>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Button } from "./components/VariantButton";
 import { Input } from "./components/Input";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
+    
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-cyan-900">
       
       <Button variant="primary">Cadastro</Button>
