f5078c41916b710a50aff8bc949282c7ba4e87a1
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-12-08 15:03:27 -0300

    wip: Context and RecentPosts FE

--- app/components/ProfileHeader.tsx
+++ app/components/ProfileHeader.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Button } from "@/app/components/VariantButton";
 
-const PROFILE_PIC_URL = "/profile-picture.jpg";
-const USERNAME = useUserContext;
-
-
 export default function ProfileHeader() {
+    /*const context = useUserContext()*/
+    const PROFILE_PIC_URL = "/profile-picture.jpg";
+    const USERNAME = /*context.username ||*/ "João Banana" ;
+    const USER_BIO = "User bio user bio";
+    const POSTS_COUNT = 33;
+    const FOLLOWERS_COUNT = 120;
+    const FOLLOWING_COUNT = 150;
+
     return (
         <div className="w-full bg-neutral-700 text-white pt-6 pb-4 px-6 border-neutral-800">
             <div className="flex items-start justify-between">
@@ -13,7 +17,7 @@ export default function ProfileHeader() {
                     {/* Avatar */}
                     <div className="w-20 h-20 rounded-full bg-neutral-800 overflow-hidden">
                         <Image
-                            src="/profile-picture.jpg"
+                            src={PROFILE_PIC_URL}
                             alt="Profile Picture"
                             width={80}
                             height={80}
@@ -23,31 +27,31 @@ export default function ProfileHeader() {
                     {/* Username + Edit Button */}
                     <div className="flex flex-col mt-5">
                         <div className="flex items-center gap-2">
-                            <h2 className="text-xl font-semibold">João Banana</h2>
+                            <h2 className="text-xl font-semibold">{USERNAME}</h2>
 
                             <Button variant="quarternary" className="text-xs py-0.5 px-3">
                                 Edit Profile
                             </Button>
                         </div>
-
-                        <p className="text-neutral-400 text-sm">User bio user bio</p>
+                        {/* User Bio */}
+                        <p className="text-neutral-400 text-sm">{USER_BIO}</p>
                     </div>
                 </div>
 
                 {/* Botões de Post, Followers e Following */}
                 <div className="flex items-center gap-10 text-right mt-5">
                     <div className="flex flex-col items-center">
-                        <Button variant="quinternary" className="text-xl py-0.5 px-3"> 33 </Button>
+                        <Button variant="quinternary" className="text-xl py-0.5 px-3"> {POSTS_COUNT} </Button>
                         <p className="text-neutral-400 text-xs">posts</p>
                     </div>
 
                     <div className="flex flex-col items-center">
-                        <Button variant="quinternary" className="text-xl py-0.5 px-3"> 120 </Button>
+                        <Button variant="quinternary" className="text-xl py-0.5 px-3"> {FOLLOWERS_COUNT} </Button>
                         <p className="text-neutral-400 text-xs">followers</p>
                     </div>
 
                     <div className="flex flex-col items-center">
-                       <Button variant="quinternary" className="text-xl py-0.5 px-3"> 150 </Button>
+                       <Button variant="quinternary" className="text-xl py-0.5 px-3"> {FOLLOWING_COUNT} </Button>
                         <p className="text-neutral-400 text-xs">following</p>
                     </div>
                 </div>
